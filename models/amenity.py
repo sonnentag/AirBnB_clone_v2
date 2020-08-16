d26c8c8b92b85559118ef0d2774befb46f52a68a
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-16 19:24:00 +0000

    reverted amenities to the way it was before merge

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel):
     '''class Amenity'''
 
-    __tablename__ = 'amenities'
-    name = Column(String(128))
-    place_amenities = relationship(
-        "Place", secondary='place_amenity', backref='amenities')
+    name = ''

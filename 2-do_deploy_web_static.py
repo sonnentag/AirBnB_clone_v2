bca50812a6fb421067b5d072723333307a9b6ff9
Tony Sims <zocle@zocle.net>
2020-08-17 23:36:38 -0500

    trying this 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,14 +19,12 @@ def do_deploy(archive_path):
     tarball = archive_path.split('/')[1]
     filename = tarball.split('.')[0]
 
-    run('mkdir -p /data/web_static/releases/{}/'.format(filename))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+    run('mkdir -p web_static/releases/{}/'.format(filename))
+    run('tar -xzf /tmp/{} -C web_static/releases/{}/'
         .format(tarball, filename))
     run('rm -rf /tmp/{}'.format(tarball))
-    path = '/data/web_static/releases'
-    run('mv {}/{}/web_static/* {}/{}'.format(path, filename, path, filename))
-    run('rm -rf /data/web_static/current')
+    run('rm /data/web_static/current')
     run('ln -s /data/web_static/releases/{}/\
-        /data/web_static/current'.format(filename))
+        /data/web_static/current/'.format(filename))
 
     return True

85424da8956ad9955442ae17e50e858400754c42
Tony Sims <zocle@zocle.net>
2020-09-03 03:51:11 +0000

    fixed typo'd comma

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,22 +3,22 @@
 
 from flask import Flask
 
-app = Flask('__name__')
+app = Flask(__name__)
 
 
-@app.route('/'. strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     ''' Display 'Hello HBNB!' '''
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb'. strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def dpl_hbnb():
     ''' Display 'HBNB' '''
     return 'HBNB'
 
 
-@app.route('/c/<text>'. strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def dpl_ctext(text):
     ''' Display 'C <text>' '''
     return 'C {}'.format(text.replace('_', ' '))

857860e07929b88febe0a5ff5f26d9a3ddca0e27
Tony Sims <zocle@zocle.net>
2020-09-03 03:40:12 +0000

    removed quotes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,13 +2,15 @@
 """ flask app """
 
 from flask import Flask
-app = Flask('__name__')
+
+app = Flask(__name__)
 
 
 @app.route('/'. strict_slashes=False)
 def hello_hbnb():
     ''' hello hbnb'''
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     ''' main '''

f3eebf41b1a1b9038f116e11b05f1df5c6343243
Tony Sims <zocle@zocle.net>
2020-09-03 23:03:30 -0400

    first push

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" flask app """
+
+from flask import Flask
+
+app = Flask('__name__')
+
+
+@app.route('/'. strict_slashes=False)
+def hello_hbnb():
+    ''' Display 'Hello HBNB!' '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb'. strict_slashes=False)
+def dpl_hbnb():
+    ''' Display 'HBNB' '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>'. strict_slashes=False)
+def dpl_ctext(text):
+    ''' Display 'C <text>' '''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    ''' main '''
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" flask app """
+
+from flask import Flask
+
+app = Flask('__name__')
+
+
+@app.route('/'. strict_slashes=False)
+def hello_hbnb():
+    ''' Display 'Hello HBNB!' '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb'. strict_slashes=False)
+def dpl_hbnb():
+    ''' Display 'HBNB' '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>'. strict_slashes=False)
+def dpl_ctext(text):
+    ''' Display 'C <text>' '''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/'. strict_slashes=False)
+@app.route('/python/<text>'. strict_slashes=False)
+def dpl_ptext(text="is cool"):
+    ''' Display 'C <text>' '''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>'. strict_slashes=False)
+def dpl_number(n):
+    ''' Display '<n> is a number' '''
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>'. strict_slashes=False)
+def dpl_ntemplate(n):
+    ''' Display 'Number: <n>' per html template '''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    ''' main '''
+    app.run(host='0.0.0.0', port=5000)
